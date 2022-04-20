80f0f1b41fbc179236fc63a7013ab3a4f04bbaa6
Gonzalo Pulgar <gonzalo.pulgar@usach.cl>
2022-04-20 00:33:33 -0400

    sitio web en spring

---
+++ src/main/java/pep/mingesoft/demo/helloWorld.java
@@ -0,0 +1,16 @@
+package pep.mingesoft.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorld {
+
+    @RequestMapping("/")
+    public String holaMundo() {
+        int b = 3;
+        int c = 1;
+        return "Hola Mundo, soy una aplicacion web super simple: " + (b + 4 - c);
+    }
+
+}
